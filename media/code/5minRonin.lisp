8a69cf5ec120c34f699da7255936ed54aa521b7d
Sean Lewis <scl63@drexel.edu>
2022-08-26 15:35:59 -0400

    added ditital art section and Ronin description

---
+++ media/code/5minRonin.lisp
@@ -0,0 +1,23 @@
+(clear)
+(resize 1000 1000)
+(fill frame "black")
+(fill 
+  (circle 158 154 262.94) 
+  (gradient 
+    (line 88 62 412 368) 
+    ("white" "black")))
+(fill 
+  (circle 830 830 500.12) 
+  (gradient 
+    (line 408 418 996 926) 
+    ("black" "white")))
+(stroke 
+  (circle 830 830 525.12) "white" 5)
+(stroke 
+  (circle 830 830 550.12) "yellow" 30)
+(stroke 
+  (circle 830 830 560.12) "black" 3)
+(stroke 
+  (circle 158 154 286.94) "yellow" 5)
+(stroke 
+  (circle 158 154 308.94) "gray" 20)
